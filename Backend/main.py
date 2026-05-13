48500e0e3916d17dac1b30f5539ed26d97bc1882
asadulla-h <27100349@lums.edu.pk>
2026-05-13 15:52:34 +0500

    HELL

--- Backend/main.py
+++ Backend/main.py
@@ -6,13 +6,18 @@ from database.vector_db import search_catalog
 from google import genai
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
+from pathlib import Path
 
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="Frontend"), name="static")
+BASE_DIR = Path(__file__).resolve().parent.parent 
+FRONTEND_DIR = BASE_DIR / "Frontend"
+
+# Mount using the absolute path
+app.mount("/static", StaticFiles(directory=str(FRONTEND_DIR)), name="static")
 
 @app.get("/")
 async def read_index():
-    return FileResponse("Frontend/index.html")
+    return FileResponse(FRONTEND_DIR / "index.html")
 
 # 1. CORS is CRITICAL: This allows your widget on ANY site to talk to your backend
 app.add_middleware(
